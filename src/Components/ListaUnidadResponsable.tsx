6e46903b9b4aebb304912ce10ecb835947f8ba05
Lupita-Hernandez <lupita27hv@gmail.com>
2025-04-19 14:49:47 -0600

    1

--- src/Components/ListaUnidadResponsable.tsx
+++ src/Components/ListaUnidadResponsable.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import UnidadResponsableService from '../Services/UnidadResponsableService';
-<<<<<<< HEAD
+
 interface UnidadResponsable {
     idUnidadResponsable: number;
     jefeUnidad: string;
@@ -27,48 +27,4 @@ export default function ListaUnidadResponsable() {
         </div>
     );
 }
-=======
 
-//define la interfaz articulo para tipar los objetos que representan a los articulos 
-
-interface UnidadResponsable {
-    idUnidadResponsable: number;
-    jefeUnidad: string;
-    nombreUnidadResponsable: string;
-}
-
-
-export default function ListaRol() {
-
-
-    //define el componente funcional lista articulos
-    const [unidadesResponsables, setUnidadesResponsables] = useState<UnidadResponsable[]>([]);
-    //Hook useEfect para realizar efectos secundarios en el componenete
-    useEffect(() => {
-        //llama al servicio articulo para obtener todos los articulos
-       UnidadResponsableService.findAll().then(Response => {
-            //actuliza el estado de las mascotas con los datos obtenidos
-            setUnidadesResponsables(Response.data);
-        }).catch(error => {
-            //imprime el error que se tuvo
-            console.log(error);
-
-        })
-
-    }, []);//El array vacio como segundo argumento indica que este efecto
-    //solo debe ejecutarse una vez al montar el componente
-
-    //renderiza el componente Datatable para mostrar la lista de mascotas
-    return (
-        <div className="card">
-            <DataTable value={unidadesResponsables} tableStyle={{ minWidth: '50rem' }}>
-                
-                <Column field='idUnidadResponsable' header="ID"></Column>
-                <Column field='nombreUnidadResponsable' header="Nombre Unidad Responsable"></Column>
-                <Column field='jefeUnidad' header="Jefe Unidad"></Column>
-            </DataTable>
-
-        </div>
-    );
-}
->>>>>>> 4cda38e (1)
--- src/Services/UnidadResponsableService.tsx
+++ src/Services/UnidadResponsableService.tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-<<<<<<< HEAD
 const URL_BASE = "http://localhost:8080/unidadResponsable";
 class UnidadResponsableService{
 
@@ -20,35 +19,3 @@ class UnidadResponsableService{
     }
 }
 export default new UnidadResponsableService();
-=======
-
-const URL_BASE = "http://localhost:8080/unidadResponsable";
-
-class UnidadResponsableService{
-
-    //metodo para obtener todos los ariculos
-    findAll(){
-        return axios.get(URL_BASE);
-    }
-
-    findUsuarioById(idUnidadResponsable: number){
-        return axios.get(URL_BASE + '/' + idUnidadResponsable)
-    }
-    //Metodo para crear una nueva mascota
-    create(unidadResponsable: object){
-        return axios.post(URL_BASE, unidadResponsable);
-    }
-
-    //Metodo para actualizar un alrticulo existente
-    update(idUnidadResponsable: number, unidadResponsable: object){
-        return axios.put(URL_BASE + '/'+idUnidadResponsable, unidadResponsable)
-    }
-
-    //metodo para eliminar un articulo
-    delete(idUnidadResponsable: number){
-        return axios.delete(URL_BASE + '/' + idUnidadResponsable)
-    }
-}
-//se exporta la clase 
-export default new UnidadResponsableService();
->>>>>>> 4cda38e (1)
