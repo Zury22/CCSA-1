5c0e5dd8724f8d140d30684fd6854b857ef8f952
Lupita-Hernandez <lupita27hv@gmail.com>
2025-04-19 14:36:07 -0600

    1

--- src/Services/UsuarioService.tsx
+++ src/Services/UsuarioService.tsx
@@ -15,18 +15,18 @@ class UsuarioService{
 
 
     //Metodo para crear una nueva mascota
-    create(rol: object){
-        return axios.post(URL_BASE, rol);
+    create(usuario: object){
+        return axios.post(URL_BASE, usuario);
     }
 
     //Metodo para actualizar un alrticulo existente
-    update(idRol: number, rol: object){
-        return axios.put(URL_BASE + '/'+idRol, rol)
+    update(idUsuario: number, usuario: object){
+        return axios.put(URL_BASE + '/'+idUsuario, usuario)
     }
 
     //metodo para eliminar un articulo
-    delete(idRol: number){
-        return axios.delete(URL_BASE + '/' + idRol)
+    delete(idUsuario: number){
+        return axios.delete(URL_BASE + '/' + idUsuario)
     }
 }
 //se exporta la clase 
